d93f3d9d027f1b13678b1365505b13e537f63889
Максим Бажин <makc.fors@yandex.ru>
2025-04-23 14:54:12 +0300

    Установлен vue

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -8,6 +9,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            // The alias from vue to vue.esm-bundler.js instructs Vite to use a version of Vue.js that also bundles the compiler which will allow us to write HTML instead of render() functions
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
